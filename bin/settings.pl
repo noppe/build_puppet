7ce1b192788f8aa52c450a55f5063d5b485c2a58
Nils Olof Paulsson <nils.olof.xo.paulsson@ericsson.com>
2013-10-09 09:29:12 +0200

    Fix OS settings for 3.3.1

--- bin/settings.pl
+++ bin/settings.pl
@@ -128,7 +128,7 @@ $hiera121 = {
     'fetch'   => 'wget http://downloads.puppetlabs.com/hiera/hiera-1.2.1.tar.gz',
     'pkgsrc'  => $top . '/tgzs/hiera-1.2.1.tar.gz',
     'srcdir'  => "${top}/${src}/hiera-1.2.1",
-    'packup'  => 'gunzip -c %PKGSRC% | tar xf -',
+    'packup'  => 'gunzip -c %PKGSRC% | tar xf - ; cp ${top}/install.rb .',
     'install' => "${prefix}/bin/ruby install.rb --no-configs",
 } ;
 
