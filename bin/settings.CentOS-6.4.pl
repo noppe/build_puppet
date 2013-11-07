5c1406db6ecfdaa8182cf5b5b087fc87e09853d1
Nils Olof Paulsson <nils.olof.xo.paulsson@ericsson.com>
2013-11-07 22:05:35 +0100

    Junk!

--- bin/settings.CentOS-6.4.pl
+++ bin/settings.CentOS-6.4.pl
@@ -1,9 +1,10 @@
 $bits = `uname -m` ;
-if ($bits eq 'i686') {
+if ($bits =~ /i686/) {
   $syst = 'linux-elf' ;
 } else {
   $syst = 'linux-x86_64' ;
 }
+
 $openssl098w = {
     %{$openssl098w},
     'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} ${syst} -static-libgcc",
