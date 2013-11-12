3ee9a54188a8550c7652fd6a6819af0413525c7a
Nils Olof Paulsson <nils.olof.xo.paulsson@ericsson.com>
2013-11-12 13:53:39 +0100

    Fix sol-(9,10)

--- bin/settings.SunOS-5.10.pl
+++ bin/settings.SunOS-5.10.pl
@@ -4,13 +4,24 @@
 
 
 if ($platform_arch eq 'sparc') {
-    $openssl101e = {
-  %{$openssl101e},
-  'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-sparcv9-gcc -static-libgcc",
-    } ;
+  $openssl101e = {
+    %{$openssl101e},
+    'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-sparcv9-gcc -static-libgcc",
+  } ;
+  $augeas110 = {
+    %{$augeas110},
+    'configure' => "./configure --prefix=${prefix} CPPFLAGS=-I${prefix}/include LDFLAGS=\"-L${prefix}/lib -R${prefix}/lib -lncurses\" CFLAGS=-static-libgcc",
+  } ; 
+
 } else {
-    $openssl101e = {
-  %{$openssl101e},
-  'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-x86-gcc -static-libgcc",
-    } ;
+  $openssl101e = {
+    %{$openssl101e},
+    'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-x86-gcc -static-libgcc",
+  } ;
+  $augeas110 = {
+    %{$augeas110},
+    'configure' => "./configure --prefix=${prefix} CPPFLAGS=-I${prefix}/include LDFLAGS=\"-L${prefix}/lib -R${prefix}/lib\ -lcurses\" CFLAGS=-static-libgcc",
+  } ; 
 }
+
+1;
--- bin/settings.SunOS-5.9.pl
+++ bin/settings.SunOS-5.9.pl
@@ -2,14 +2,16 @@
 # Solaris settings
 #
 
-if ($platform_arch eq 'sparc') {
-    $openssl101e = {
-  %{$openss101e},
-  'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-sparcv9-gcc -static-libgcc",
-    } ;
+if ($platform_arch =~ /sparc/) {
+  $openssl101e = {
+    %{$openssl101e},
+    'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-sparcv9-gcc -static-libgcc",
+  } ;
 } else {
-    $openssl101e = {
-  %{$openssl101e},
-  'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-x86-gcc -static-libgcc",
-    } ;
+  $openssl101e = {
+    %{$openssl101e},
+    'configure' => "./Configure  -L${prefix}/lib -I${prefix}/include -R${prefix}/lib shared zlib-dynamic --prefix=${prefix} --openssldir=${prefix} solaris-x86-gcc -static-libgcc",
+  } ;
 }
+
+1;
