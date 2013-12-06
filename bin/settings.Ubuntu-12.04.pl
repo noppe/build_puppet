a024ec0a1a4faa20aa2fb217010d875b1bd14560
Andrew Bastien <andrew.bastien@ericsson.com>
2013-12-06 12:54:27 -0800

    Fix ruby-augeas build on Ubuntu

--- bin/settings.Ubuntu-12.04.pl
+++ bin/settings.Ubuntu-12.04.pl
@@ -49,11 +49,12 @@ $augeas110 = {
     'install'   => 'make install',
 } ;
 
+#    'configure' => "cd ext/augeas ; echo \"require 'mkmf' ; extension_name = '_augeas' ; create_makefile(extension_name)\" > ee2.rb ; ${prefix}/bin/ruby ee2.rb ; cd ../..",
 $ruby_augeas050 = {
     %{$ruby_augeas050},
-    'configure' => "cd ext/augeas ; echo \"require 'mkmf' ; extension_name = '_augeas' ; create_makefile(extension_name)\" > ee2.rb ; ${prefix}/bin/ruby ee2.rb ; cd ../..",
-    'make'      => 'cd ext/augeas ; make CC="gcc -I/usr/include/libxml2 -laugeas" ; cd ../..',
-    'install'   => "cp lib/augeas.rb ${prefix}/lib/ruby/site_ruby/1.8 ; cd ext/augeas ; make install; ",
+    'configure' => "cd ext/augeas ; ${prefix}/bin/ruby extconf.rb ; cd ../..",
+    'make'      => 'cd ext/augeas ; cd ../..',
+    'install'   => "cp lib/augeas.rb ${prefix}/lib/ruby/site_ruby/1.8 ; cd ext/augeas ; make install",
 } ;
 
 $rubyshadow214 = {
